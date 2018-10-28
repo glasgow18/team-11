b0910213908e0889e59ec055336244e191e72502
Catalina Rotaru <rotarucatalina991@gmail.com>
2018-10-28 09:49:25 +0000

    New features

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class MessageService {
-  messages: string[] = ['HI','How are you?', 'I am tired'];
+  messages: string[] = ['Hi','How are you?', 'I am tired'];
 
   add(message: string) {
     this.messages.push(message);
