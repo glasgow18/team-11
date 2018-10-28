aaddca8f6f40c3fc5be52e434fa55beada809df8
Teodor Yanev <43523832+TeodorYanev1@users.noreply.github.com>
2018-10-28 01:09:41 +0100

    Add files via upload
    "weAreTheMostAgileTeam#4members#swag"

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+import { MainComponent } from './main/main.component';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule
+  ],
+  declarations: [ AppComponent, MainComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
